8ae3ef6f1fafa13c16a3d2495fef7dbe478ea932
Mutebu <ms18.cgafv@gmail.com>
2025-06-17 15:38:57 +0400

    done

---
+++ buttons.js
@@ -0,0 +1,11 @@
+const htmlSkill = document.getElementById('htmlSkill');
+const cssSkill = document.getElementById('cssSkill');
+const jsSkill = document.getElementById('jsSkill');
+const githubSkill = document.getElementById('githubSkill');
+const gitSkill = document.getElementById('gitSkill');
+
+htmlSkill.onclick=function(){htmlSkill.innerHTML = 'I can do basic websites and make them responsive'}
+cssSkill.onclick=function(){cssSkill.innerHTML = 'I can make a website beautiful'}
+jsSkill.onclick=function(){jsSkill.innerHTML = 'I can make a website interactive'}
+githubSkill.onclick=function(){githubSkill.innerHTML = 'I can publish my work and work in a team'}
+gitSkill.onclick=function(){gitSkill.innerHTML = 'I can work with different branches and I know some commands'}
